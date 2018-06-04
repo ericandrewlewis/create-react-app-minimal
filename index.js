4c4416089dfde2b1066cf351afe555b99ca66ee8
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-04 13:21:38 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const { spawn } = require("child_process");
+const path = require("path");
+const program = require("commander");
+const fse = require("fs-extra");
+
+let projectName;
+
+program
+  .arguments("<project-directory>")
+  .action(name => {
+    projectName = name;
+  })
+  .allowUnknownOption()
+  .parse(process.argv);
+
+const argumentsForCreateReactApp = process.argv.splice(2);
+
+const createReactAppProcess = spawn(
+  "create-react-app",
+  argumentsForCreateReactApp
+);
+
+process.stdout.write("\n    Running create-react-app...\n");
+
+createReactAppProcess.stdout.on("data", data => {
+  process.stdout.write(data);
+});
+
+createReactAppProcess.stderr.on("data", data => {
+  process.stderr.write(data);
+});
+
+createReactAppProcess.on("close", code => {
+  if (code !== 0) {
+    return;
+  }
+  const filesToDelete = [""];
+
+  fse.remove(path.join(projectName, "src"), err => {
+    if (err) {
+      return console.error(err);
+    }
+    fse.copy("minimalSrcContent", path.join(projectName, "src"), err => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("Complete");
+    });
+  });
+});
